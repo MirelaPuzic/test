901dcc786ce4e176d37bf2456ebe8386e23167fa
Mirela Puzic <mirela.vekic@gmail.com>
2022-01-24 21:15:42 +0100

    Register a new pos type

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php
+/*Register a new post type,*/
+
+function create_posttype() {
+  register_post_type( 'wpll_dealers',
+    array(
+      'labels' => array(
+        'name' => __( 'Dealers' ),
+        'singular_name' => __( 'Dealers' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'dealers'),
+    )
+  );
+}
+
+add_action( 'init', 'create_posttype' );
+
+?>
