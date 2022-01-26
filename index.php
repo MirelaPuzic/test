9f19232613be857bc9546bb9a5a54bcd909dd661
Mirela Puzic <mirela/vekicgmail.com>
2022-01-26 23:33:24 +0100

    Table for data

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ add_action( 'init', 'DealersPostType' );
  //Table for data
  CREATE TABLE `locations` (
 `id` INT(10) NOT NULL AUTO_INCREMENT,
+`title` VARCHAR(150) NOT NULL,
 `address` VARCHAR(255) NOT NULL,
 `lat` FLOAT(10,6) NOT NULL,
 `lon` FLOAT(10,6) NOT NULL,
@@ -32,7 +33,9 @@ PRIMARY KEY (`id`)
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 
-INSERT INTO `locations` (`id`, `address`, `lat`, `lon`, )
- VALUES (1, '4306 3rd AVE  BROOKLYN NY 11232', 40.6517653, -74.0110184 ');
- INSERT INTO `locations` (`id`, `address`, `lat`, `lon`, )
-  VALUES (1, '4306 3rd AVE  BROOKLYN NY 11232', 40.6517653, -74.0110184 ');
+INSERT INTO `locations` (`id`, `title`, `address`, `lat`, `lon`, )
+ VALUES (1, '3rd AVE CABINET & TILE INC.', '4306 3rd AVE  BROOKLYN NY 11232',
+  40.6517653, -74.0110184 );
+INSERT INTO `locations` (`id`, `title`, `address`, `lat`, `lon`, )
+  VALUES (2, 'A & R KITCHEN & BATH SHOWROOM INC.', '881 BEDFORD AVE.  BROOKLYN NY 11205',
+    40.6936857, -73.955752 );
