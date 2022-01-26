073b029ee4c6644c61c2abc458d4d83075dd6796
Mirela Puzic <mirela/vekicgmail.com>
2022-01-26 22:38:21 +0100

    Register post type

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
-function create_posttype() {
-  register_post_type( 'wpll_dealers',
+
+function DealerstPostType() {
+register_post_type( 'dealers',
     array(
       'labels' => array(
         'name' => __( 'Dealers' ),
         'singular_name' => __( 'Dealer' )
       ),
       'public' => true,
-      'has_archive' => true,
+      'has_archive' => 'themes',
       'rewrite' => array('slug' => 'dealers'),
+      'supports' => array( 'title', 'editor', 'thumbnail', 'excerpt'),
+      'description' => 'Custom Theme Posts',
+      'show_ui' => true,
+      'show_in_menu' => true,
     )
-  );
+);
 }
-add_action( 'init', 'create_posttype' );
-
-
-
+add_action( 'init', 'DealersPostType' );
  ?>
